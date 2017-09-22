f6fb788924d133bd6adb00cc84ee6669b2d7a1d1
Tom Schuster <tomjschuster@gmail.com>
2017-09-21 23:58:40 -0400

    init with basic elm app

---
+++ src/Main.elm
@@ -0,0 +1,45 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, h1, text)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    Model ! []
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Sequelize UI" ]
+        ]
