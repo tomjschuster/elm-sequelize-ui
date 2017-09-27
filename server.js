6e6eb7210298236626d3992500bd0251e8da9120
Tom Schuster <tomjschuster@gmail.com>
2017-09-26 20:07:09 -0400

    simplify server

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const express = require('express')
-const path = require('path')
-const app = express()
-
-app.use(express.static(__dirname))
-
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
-
-app.listen(3000, () => console.log('Listening at 3000'))
+express()
+  .use(express.static(__dirname))
+  .get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
+  .listen(3000, () => console.log('Listening at 3000'))
