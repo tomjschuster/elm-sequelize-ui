14be794be98463ef2885214d6083c1bb94402011
Tom Schuster <tomjschuster@gmail.com>
2017-10-22 22:29:41 -0400

    validate default value

--- lib/sequelize_ui/db_design/constraint.ex
+++ lib/sequelize_ui/db_design/constraint.ex
@@ -1,6 +1,7 @@
 defmodule SequelizeUi.DbDesign.Constraint do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Ecto.Changeset
   alias SequelizeUi.DbDesign.{Constraint, ConstraintType, Schema, Table, Column, ColumnConstraint}
 
 
@@ -26,9 +27,20 @@ defmodule SequelizeUi.DbDesign.Constraint do
     constraint
     |> cast(attrs, [:name, :constraint_type_id, :schema_id, :table_id, :value])
     |> validate_required([:constraint_type_id, :schema_id, :table_id])
+    |> validate_default_value()
     |> assoc_constraint(:constraint_type)
     |> assoc_constraint(:schema)
     |> assoc_constraint(:table)
     |> unique_constraint(:name, name: :sql_constraint_schema_id_name_index)
+  end
+
+  defp validate_default_value(%Changeset{} = changeset) do
+    IO.inspect(changeset)
+    validate_change(changeset, :constraint_type_id, fn _, type_id ->
+      case {type_id, changeset.data.value} do
+        {3, nil} -> [{:value, "can't be blank"}]
+        _ -> []
+      end
+    end)
   end
 end
